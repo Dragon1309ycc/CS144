fd2046b17d78ba0d78cfc4154d513208ce96153c
yc <yangchen1309@163.com>
2025-03-31 12:00:56 +0800

    Lab 2 doing

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -3,16 +3,14 @@
 
 using namespace std;
 
-Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
+Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point ) //这里的zerp_point 就是 ISN 初始序列号
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  //将绝对序列号转换成序列号
+  return  static_cast<Wrap32>(n) + zero_point.raw_value_;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
+  
   return {};
 }
